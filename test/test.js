a21e109345e077ee72166bb0fa28e3040b3343fd
Lang Tran <lang.tran96@gmail.com>
2019-04-07 13:47:05 -0700

    secondary test for simultaneous connections, new packages for testing

--- test/test.js
+++ test/test.js
@@ -2,8 +2,7 @@ var assert = require('assert');
 var nodeServer = require('../src/server.js');
 var io = require('socket.io-client');
 var config = require('../config/config.js');
-
-
+var sinon = require('sinon');
 
 const url = `http://${config.app.host}:${config.app.port}`;
 describe('/', () => {
@@ -26,6 +25,34 @@ describe('/', () => {
             assert.equal(msg, testMsg);
             socket1.close();
             socket2.close();
+            done();
+        });
+
+        socket1.emit('update', testMsg);
+    });
+    it('should be able to handle simultaneous connections', (done) => {
+        var socket1 = io.connect(url);
+        var socket2 = io.connect(url);
+        var socket3 = io.connect(url);
+        var socket4 = io.connect(url);
+
+        var testMsg = 'test';
+        var testMsg2 = 'test2';
+        var spy = sinon.spy();
+
+        socket2.on('update', (msg) => {
+            spy();
+            assert.equal(msg, testMsg);
+            socket3.emit('update', testMsg2);
+            socket1.close();
+            socket2.close();
+        });
+        socket4.on('update', (msg) => {
+            spy();
+            assert.equal(msg, testMsg);
+            socket3.close();
+            socket4.close();
+            sinon.assert.calledTwice(spy);
             done();
         });
 
