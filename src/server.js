8c778d50a7e42d0a92510e3807e129be5dee2a22
Hans Zheng <hzheng44@berkeley.edu>
2019-04-22 17:44:57 -0700

    Change to express server and add cors

--- src/server.js
+++ src/server.js
@@ -1,22 +1,31 @@
 var http = require('http');
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
 var fs = require('fs');
 var serverIo = require('socket.io');
 var config = require('../config/config');
 
+
+app.use(cors());
+var server = require('http').Server(app);
+
 class Server {
     constructor() {
-        this.server = http.createServer(
-            function(req, res) {
-            fs.readFile('./pageindex.html', 
-                'utf-8', function(error, content) {
-                    res.setHeader("Content-Type", "text/html")
-                    res.setHeader("Access-Control-Allow-Credentials", true)
-                    res.setHeader("Access-Control-Allow-Origin", "*");
-                    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-                    res.statusCode = 200;
-                    res.end(content);
-                });
-        });
+        this.server = server;
+        // this.server = http.createServer(
+        //     function(req, res) {
+        //     fs.readFile('./pageindex.html', 
+        //         'utf-8', function(error, content) {
+        //             res.setHeader("Content-Type", "text/html")
+        //             res.setHeader("Access-Control-Allow-Credentials", true)
+        //             res.setHeader("Access-Control-Allow-Origin", "*");
+        //             res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //             res.statusCode = 200;
+        //             res.end(content);
+        //         });
+        // });
         this.io = serverIo(this.server, { origin: '*:*', transport:['websocket']});
         this.sockMap= {};
     }
